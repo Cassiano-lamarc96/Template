7263c2d724fe372de912b2ce09be11d0722aeb4b
karatekid <karatepontokid@gmail.com>
2021-01-10 20:18:49 -0300

    Instalando e Configurando o EntityFramework Core

--- Template/Startup.cs
+++ Template/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Template.Data.Context;
 
 namespace Template
 {
@@ -25,6 +27,7 @@ namespace Template
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TemplateContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("TemplateDB")).EnableSensitiveDataLogging());
             services.AddControllers();
         }
 
